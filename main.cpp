48fe2d5bde4f83cc7022bc29f865a85eefde4d50
Kai Tung Yu <sam@bristolgram.uk>
2025-10-11 14:41:06 +0100

    new template

--- main.cpp
+++ main.cpp
@@ -1,22 +1,17 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
-#include <QtWidgets/QApplication>
-
-#include "SerialPort.h"
-
-using namespace Qt::Literals::StringLiterals;
 
 int main(int argc, char *argv[])
 {
-    QApplication app(argc, argv);
-
+    QGuiApplication app(argc, argv);
     QQmlApplicationEngine engine;
-    qmlRegisterSingletonType<SerialPort>("SerialPort", 1, 0, "SerialPort", &SerialPort::qmlInstance);
-    const QUrl url(u"qrc:/LGDXRobot2-ChassisTuner/Main.qml"_s);
-    QObject::connect(&engine, &QQmlApplicationEngine::objectCreationFailed,
-        &app, []() { QCoreApplication::exit(-1); },
+    QObject::connect(
+        &engine,
+        &QQmlApplicationEngine::objectCreationFailed,
+        &app,
+        []() { QCoreApplication::exit(-1); },
         Qt::QueuedConnection);
-    engine.load(url);
+    engine.loadFromModule("LGDXRobot2ChassisTuner", "Main");
 
     return app.exec();
 }
